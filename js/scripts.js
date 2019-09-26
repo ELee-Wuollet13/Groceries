b56753cb3f75fae2d92ad973436eeafaaab76a25
Student <example@example.com>
2019-09-25 19:32:27 -0700

    complete html and js

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $(".formGroup form").submit(function(event) {
+
+    var array = [];
+
+    // var userInput = ["#input1", "#input2", "#input3", "#input4", "#input5"];
+    //
+    // userInput.forEach(function(input) {
+    //   var user = $("input" + imput)
+    //   // ray.push($(userInput))
+    // })
+    array.push($("#input1").val());
+    array.push($("#input2").val());
+    array.push($("#input3").val());
+    array.push($("#input4").val());
+    array.push($("#input5").val());
+    console.log(array);
+    var returnValue = array.map(function(array1) {
+      console.log("array1: " + array1);
+      return array1.toUpperCase();
+    });
+      returnValue.sort();
+    console.log(returnValue);
+    event.preventDefault();
+  });
+});
